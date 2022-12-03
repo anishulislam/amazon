482e1498aee7f5ca0e72ef17940455c9f7606ac4
Mohammed anishul islam <115714071+anishulislam@users.noreply.github.com>
2022-12-03 21:01:24 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,2 +1,10 @@
-let username="abcdef";
-let password="abc123";
+let num=13;
+for(let i=0;i<num;i++){
+  if(num%i==0){
+    count++;
+  }
+}
+if (count==3){
+  console.log("prime")
+}else{
+  console.log("not)
